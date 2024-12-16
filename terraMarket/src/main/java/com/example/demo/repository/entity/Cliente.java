b5e25ab021a579e5b1cba4ab28a0c209efb51061
Mezcu15 <154076912+Mezcu15@users.noreply.github.com>
2024-12-16 11:56:37 +0100

    Mapeo de Entidades sin relaciones

--- terraMarket/src/main/java/com/example/demo/repository/entity/Cliente.java
+++ terraMarket/src/main/java/com/example/demo/repository/entity/Cliente.java
@@ -1,5 +1,37 @@
 package com.example.demo.repository.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+
+    private String apellidos;
+    
+    private String username;
+    @Column(name = "fechanacimiento")  
+    private Date fechaNacimiento;
+
+    private String email;
+
+    private String contrasena;
+
+    private String cp;
+
+    private boolean activo;
 
 }
--- terraMarket/src/main/java/com/example/demo/repository/entity/Direccion.java
+++ terraMarket/src/main/java/com/example/demo/repository/entity/Direccion.java
@@ -1,5 +1,44 @@
 package com.example.demo.repository.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "direcciones")
 public class Direccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String pais;
+
+    private String provincia;
+
+    private String ciudad;
+
+    private String cp;
+
+    private String calle;
+
+    private String numero;
+
+    private String puerta;
+
+    private String piso;
+
+    @Column(name = "es_mercado")
+    private boolean esMercado;
+
+    @Column(name = "es_tienda")
+    private boolean esTienda;
+
+    @Column(name = "es_pr")
+    private boolean esPr;
 
 }
