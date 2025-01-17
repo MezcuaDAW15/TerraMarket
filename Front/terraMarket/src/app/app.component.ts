f64fe3027a95235660017a56a3230846a6fc2f69
Mezcu15 <154076912+Mezcu15@users.noreply.github.com>
2025-01-17 10:43:09 +0100

    Abandono de rama
    Angular 19 da conflicto

--- Front/terraMarket/src/app/app.component.ts
+++ Front/terraMarket/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { RegistroComponent } from "./registro/registro.component";
-import { PruebasComponent } from "./pruebas/pruebas.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegistroComponent } from "./components/registro/registro.component";
+import { PruebasComponent } from "./components/pruebas/pruebas.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistroComponent],
+  imports: [RouterOutlet, RegistroComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
