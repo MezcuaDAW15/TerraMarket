08674b96ce69edf3f8f371c36fde5c6f0bc3d0a2
andreaac18 <andceblop2@alu.edu.gva.es>
2025-01-18 21:08:35 +0100

    Imports
    This reverts commit 078ebc2d09b42ccfa88a395eed88cc87d3b97d17.

--- Backend/terraMarket/src/main/java/com/example/demo/repository/entity/Venta.java
+++ Backend/terraMarket/src/main/java/com/example/demo/repository/entity/Venta.java
@@ -13,6 +13,10 @@ import jakarta.persistence.Id;
 
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
 
 
 @Data
