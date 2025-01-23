e85a7e2105e5f2d29d0d0f75582eafd3414eb449
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-01-23 18:56:08 +0100

    Agregar vista de mercado con componentes de tarjeta, lista y banner

--- Front/terraMarket/src/app/app.component.ts
+++ Front/terraMarket/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { CardComponent } from "./components/card/card.component";
+import { CardComponent } from "./views/components/card/card.component";
 
 @Component({
   selector: 'app-root',
--- Front/terraMarket/src/app/components/sidebar/sidebar.component.ts
+++ Front/terraMarket/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { Sidebar } from 'primeng/sidebar';
+import { DividerModule } from 'primeng/divider';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule],
+  imports: [CommonModule, SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, DividerModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
@@ -21,4 +24,17 @@ export class SidebarComponent {
   }
 
   sidebarVisible: boolean = false;
+
+  categories = [
+    {
+      label: 'Categoría',
+
+      subcategories: [
+        { label: 'Aves y derivados', icon: 'Aves Derivados.svg' },
+        { label: 'Carnicería', icon: 'Carniceria.svg' },
+        { label: 'Charcutería y quesos', icon: 'Charcuteria.svg' },
+        { label: 'Mariscos y pescados', icon: 'Mariscos y pescados.svg' }
+      ]
+    }
+  ];
 }
