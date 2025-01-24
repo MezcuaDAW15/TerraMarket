70b9cb37812c91d1fc5bbd00874f33b82f3caf19
Mezcu15 <154076912+Mezcu15@users.noreply.github.com>
2025-01-24 10:09:39 +0100

    Update Producto.java

--- Backend/terraMarket/src/main/java/com/example/demo/repository/entity/Producto.java
+++ Backend/terraMarket/src/main/java/com/example/demo/repository/entity/Producto.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -17,20 +19,20 @@ import lombok.ToString;
 @Entity
 @Table(name = "productos")
 public class Producto {
-    @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Long id;
-    private String nombre;
-    private String imagen;
+	@Id
+	@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String imagen;
 
-  
-    
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "producto")
-    @ToString.Exclude
-    private Set<Venta> listaVentas;
-    
+	@ManyToOne
+	@JoinColumn(name = "idcategoria")
+	private CategoriaP categoriaP;
 
-  
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "producto")
+	@ToString.Exclude
+	private Set<Venta> listaVentas;
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -42,11 +44,10 @@ public class Producto {
 		Producto other = (Producto) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
-    
 }
-
