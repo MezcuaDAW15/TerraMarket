b241dec242d4d7a211fc29668274ca83c5345528
Mezcu15 <154076912+Mezcu15@users.noreply.github.com>
2025-01-24 09:51:29 +0100

    directorios arreglados

--- Front/terraMarket/src/app/app.component.ts
+++ Front/terraMarket/src/app/app.component.ts
@@ -1,13 +1,13 @@
+import { CardComponent } from './components/card/card.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { CardComponent } from "./views/components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CardComponent, CardComponent],
+  imports: [RouterOutlet, HeaderComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
