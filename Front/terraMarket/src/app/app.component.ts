12692dded5f8564bc16ea1462214cdf701ea3851
Mezcu15 <154076912+Mezcu15@users.noreply.github.com>
2025-01-27 12:21:13 +0100

    Implement market view component and update app layout; add banner and back sections

--- Front/terraMarket/src/app/app.component.ts
+++ Front/terraMarket/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { PedidosComponent } from "./components/pedidos/pedidos.component";
 import { BackComponent } from './components/back/back.component';
+import { MarketViewComponent } from "./components/market-view/market-view.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CardComponent, PedidosComponent, BackComponent],
+  imports: [RouterOutlet, HeaderComponent, CardComponent, PedidosComponent, BackComponent, MarketViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Front/terraMarket/src/app/components/market-view/market-view.component.ts
+++ Front/terraMarket/src/app/components/market-view/market-view.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BackComponent } from "../back/back.component";
+import { BannerComponent } from "../banner/banner.component";
+import { ListComponent } from "../list/list.component";
 
 @Component({
   selector: 'app-market-view',
   standalone: true,
-  imports: [],
+  imports: [BackComponent, BannerComponent, ListComponent],
   templateUrl: './market-view.component.html',
   styleUrl: './market-view.component.scss'
 })
