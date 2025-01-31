a3d091ebb222436d1afa7401cc9acab3c44790d2
Andrea <andceblop2@alu.edu.gva.es>
2025-01-31 13:53:02 +0100

    editar cliente componente - nuevo

--- Front/terraMarket/src/app/app.routes.ts
+++ Front/terraMarket/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import path from 'node:path';
 import { PedidosComponent } from './components/pedidos/pedidos.component';
 import { DetallesPedidoComponent } from './components/detalles-pedido/detalles-pedido.component';
+import { EditarPerfilClienteComponent } from './components/editar-perfil-cliente/editar-perfil-cliente.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/pedidos', pathMatch:'full'},
     {path: 'pedidos', component: PedidosComponent },
     {path: 'detalles-pedido', component: DetallesPedidoComponent },
+    {path: 'editar-perfil-cliente', component: EditarPerfilClienteComponent },
 ];
