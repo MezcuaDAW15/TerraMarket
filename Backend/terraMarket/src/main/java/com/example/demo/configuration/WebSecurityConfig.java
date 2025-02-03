b869d5b2b23a26742ba8df8a9ff33c812a719e2b
andreaac18 <andceblop2@alu.edu.gva.es>
2025-02-03 18:38:22 +0100

    El login sigue funcionando

--- Backend/terraMarket/src/main/java/com/example/demo/configuration/WebSecurityConfig.java
+++ Backend/terraMarket/src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.demo.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -15,6 +18,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+        @Autowired
+        private UserDetailsService userDetailsService;
         
 	@Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,26 +32,12 @@ public class WebSecurityConfig {
                                 .defaultSuccessUrl("/clientes", true)
                                 .failureUrl("/login?error")
                                 .permitAll()).logout(logout -> logout.logoutSuccessUrl("/").permitAll()).build();
+                
         }
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        }
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-                UserDetails user1 = User.builder()
-                                .username("jpperez")
-                                .password(passwordEncoder().encode("1234"))
-                                .roles("USER")
-                                .build();
-
-                UserDetails user2 = User.builder()
-                                .username("juan123")
-                                .password("secure123")
-                                .roles("ADMIN", "USER")
-                                .build();
-                return new InMemoryUserDetailsManager(user1, user2);
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder build) throws Exception{
+                build.userDetailsService(userDetailsService).passwordEncoder(new
+                BCryptPasswordEncoder());
         }
 }
---
+++ Backend/terraMarket/src/main/java/com/example/demo/repository/entity/Rol.java
@@ -0,0 +1,53 @@
+package com.example.demo.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "idusuario")
+    private Usuario usuario;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Rol other = (Rol) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    
+}
