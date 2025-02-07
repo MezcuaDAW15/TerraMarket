20757f6c90fc7827e21b3ff84701d0d07c9597ce
Veronica <vergimsan@alu.edu.gva.es>
2025-02-07 13:23:23 +0100

    Componente add tienda

--- Front/terraMarket/src/app/app.routes.ts
+++ Front/terraMarket/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { PedidosComponent } from './components/pedidos/pedidos.component';
 import { DetallesPedidoComponent } from './components/detalles-pedido/detalles-pedido.component';
 import { EditarPerfilClienteComponent } from './components/editar-perfil-cliente/editar-perfil-cliente.component';
 import { MercadosComponent } from './components/mercados/mercados.component';
+import { AddTiendaComponent } from './components/add-tienda/add-tienda.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/pedidos', pathMatch:'full'},
-    {path: 'pedidos', component: PedidosComponent },
-    {path: 'mercados', component: MercadosComponent },
-    {path: 'detalles-pedido', component: DetallesPedidoComponent },
-    {path: 'editar-perfil-cliente', component: EditarPerfilClienteComponent },
-    {path: 'pedido-datos-personales', redirectTo: '/pedido-datos-personales'},
+  { path: '', redirectTo: '/mercados', pathMatch: 'full' },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'mercados', component: MercadosComponent },
+  { path: 'add-tienda', component: AddTiendaComponent },
+  { path: 'detalles-pedido', component: DetallesPedidoComponent },
+  { path: 'editar-perfil-cliente', component: EditarPerfilClienteComponent },
+  { path: 'pedido-datos-personales', redirectTo: '/pedido-datos-personales' },
 ];
