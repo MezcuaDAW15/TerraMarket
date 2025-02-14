5d47b0efa59e8d893fb93928f68d90e5f6b7c75e
Andrea <andceblop2@alu.edu.gva.es>
2025-02-14 12:26:54 +0100

    Metodos pago

--- Front/terraMarket/src/app/components/realizar-pedido/components/elegir-metodo-pago/elegir-metodo-pago.component.ts
+++ Front/terraMarket/src/app/components/realizar-pedido/components/elegir-metodo-pago/elegir-metodo-pago.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MetodopagoService } from '../../../../services/metodo-pago/metodopago.service';
+import { MetodoPago } from '../../../../models/metodoPago';
 
 @Component({
   selector: 'app-elegir-metodo-pago',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './elegir-metodo-pago.component.html',
   styleUrl: './elegir-metodo-pago.component.scss'
 })
-export class ElegirMetodoPagoComponent {
+export class ElegirMetodoPagoComponent implements OnInit{
+  metodos: MetodoPago[]= [];
 
+  constructor(private metodosService : MetodopagoService){}
+  ngOnInit(): void {
+    this.mostrarMetodos()
+  }
+
+  mostrarMetodos():void{
+    this.metodosService.findAll().subscribe((data) =>{
+      this.metodos = data;
+      
+      console.log(this.metodos)
+    })
+  }
 }
